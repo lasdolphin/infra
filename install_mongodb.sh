0c7ce254a6a7015597fc0171a8e0383cc35f5422
Andrey Muravyev <lasdolphin@gmail.com>
2017-09-04 14:52:05 +0300

    space removed from expected value

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-MONGO_CHECK1_EXPECTED=" enabled"
+MONGO_CHECK1_EXPECTED="enabled"
 MONGO_CHECK2_EXPECTED="active (running)"
 # sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 # sudo bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list'

be4123adfb982d0619428bfe42c4b6b73a07736c
Andrey Muravyev <lasdolphin@gmail.com>
2017-09-04 15:38:29 +0300

    debug port value

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,7 @@ git clone https://github.com/Artemmkin/reddit.git
 cd reddit && bundle install
 puma -d
 PORT=`ps aux|grep -m1 puma| awk '{print $13}'| tr -d '()'| awk 'BEGIN { FS = ":" } ; { print $3 }'`
+echo $PORT
 # gcloud compute firewall-rules create default-puma-server --allow tcp:$PORT --source-tags=puma-server --source-ranges=0.0.0.0/0 --description="puma server port rule"
 EXTIP=`curl ipecho.net/plain`
 echo "application deployed succesfully."

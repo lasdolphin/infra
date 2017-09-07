0cbbdad712698789119b4989f03b5896b6dcd0af
Andrey Muravyev <lasdolphin@gmail.com>
2017-09-07 13:50:57 +0300

    added packer extra options + var-file+ gcloud command

---
+++ packer/scripts/deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "CD to HOME"
+cd ~
+echo `pwd`
+echo "source ~/.rvm/scripts/rvm "
+source ~/.rvm/scripts/rvm
+
+git clone https://github.com/Artemmkin/reddit.git
+cd reddit && bundle install
+puma -d
+echo "port detection"
+echo `ps aux|grep -m1 puma`
+PORT=`ps aux|grep -m1 puma| awk '{print $13}'| tr -d '()'| awk 'BEGIN { FS = ":" } ; { print $3 }'`
+echo $PORT
+EXTIP=`curl ipecho.net/plain`
+echo "application deployed succesfully."
+echo "you can connect and check."
+echo "http://$EXTIP:$PORT"

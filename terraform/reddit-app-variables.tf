c64cc0f42089d3d45837b005fc51c87f9990a650
Andrey Muravyev <lasdolphin@gmail.com>
2017-09-08 13:24:13 +0300

    ferraform fmt added

--- terraform/reddit-app-variables.tf
+++ terraform/reddit-app-variables.tf
@@ -1,16 +1,20 @@
 variable project {
-    description = "Project ID"
+  description = "Project ID"
 }
+
 variable region {
-    description = "Region"
-    default = "europe-west1"
+  description = "Region"
+  default     = "europe-west1"
 }
+
 variable public_key_path {
-    description = "path to publick key for ssh"
+  description = "path to publick key for ssh"
 }
+
 variable private_key_path {
-    description = "path to publick key for ssh"
+  description = "path to publick key for ssh"
 }
-variable disk_image{
-    description = "Disk Image"
+
+variable disk_image {
+  description = "Disk Image"
 }

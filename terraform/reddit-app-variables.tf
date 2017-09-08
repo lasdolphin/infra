8230858317a9c454c25eb464c5111becb38a1ba3
Andrey Muravyev <lasdolphin@gmail.com>
2017-09-08 13:22:02 +0300

    terraform homework

---
+++ terraform/reddit-app-variables.tf
@@ -0,0 +1,16 @@
+variable project {
+    description = "Project ID"
+}
+variable region {
+    description = "Region"
+    default = "europe-west1"
+}
+variable public_key_path {
+    description = "path to publick key for ssh"
+}
+variable private_key_path {
+    description = "path to publick key for ssh"
+}
+variable disk_image{
+    description = "Disk Image"
+}
